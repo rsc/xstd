576a5513fa16fdc7f047a8bfffd48e7598045014
Russ Cox <rsc@golang.org>
2022-09-28 15:31:31 -0400

    go1.11.9: add Go 1.11.9

---
+++ go1.11.9/go.mod
@@ -0,0 +1,3 @@
+module rsc.io/xstd/go1.11.9
+
+go 1.11
---
+++ go1.11.9/internal/cpu/cpu_ppc64x_test.go
@@ -0,0 +1,35 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build ppc64 || ppc64le
+// +build ppc64 ppc64le
+
+package cpu_test
+
+import (
+	"testing"
+
+	. "rsc.io/xstd/go1.11.9/internal/cpu"
+)
+
+func TestPPC64minimalFeatures(t *testing.T) {
+	if !PPC64.IsPOWER8 {
+		t.Fatalf("IsPOWER8 expected true, got false")
+	}
+	if !PPC64.HasVMX {
+		t.Fatalf("HasVMX expected true, got false")
+	}
+	if !PPC64.HasDFP {
+		t.Fatalf("HasDFP expected true, got false")
+	}
+	if !PPC64.HasVSX {
+		t.Fatalf("HasVSX expected true, got false")
+	}
+	if !PPC64.HasISEL {
+		t.Fatalf("HasISEL expected true, got false")
+	}
+	if !PPC64.HasVCRYPTO {
+		t.Fatalf("HasVCRYPTO expected true, got false")
+	}
+}
