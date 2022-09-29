216d4681583016efcb5fa36dbae203f6de71729e
Russ Cox <rsc@golang.org>
2022-09-29 08:56:23 -0400

    go1.8.3: update Go 1.8.3

--- go1.8.3/unicode/maketables.go
+++ go1.8.3/unicode/maketables.go
@@ -14,17 +14,17 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 
+	"rsc.io/xstd/go1.8.3/io"
 	"rsc.io/xstd/go1.8.3/regexp"
 	"rsc.io/xstd/go1.8.3/sort"
 	"rsc.io/xstd/go1.8.3/strconv"
+	"rsc.io/xstd/go1.8.3/strings"
 	"rsc.io/xstd/go1.8.3/unicode"
 )
 
