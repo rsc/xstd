b48414b00bcecf1f2c58a46f4f67fc12be854c30
Russ Cox <rsc@golang.org>
2022-09-28 21:23:19 -0400

    go1.14.14: update Go 1.14.14

--- go1.14.14/internal/cpu/cpu_s390x_test.go
+++ go1.14.14/internal/cpu/cpu_s390x_test.go
@@ -6,10 +6,10 @@ package cpu_test
 
 import (
 	"errors"
-	"io/ioutil"
 	"testing"
 
 	. "rsc.io/xstd/go1.14.14/internal/cpu"
+	"rsc.io/xstd/go1.14.14/io/ioutil"
 	"rsc.io/xstd/go1.14.14/regexp"
 )
 
--- go1.14.14/regexp/onepass_test.go
+++ go1.14.14/regexp/onepass_test.go
@@ -6,10 +6,10 @@ package regexp
 
 import (
 	"reflect"
-	"strings"
 	"testing"
 
 	"rsc.io/xstd/go1.14.14/regexp/syntax"
+	"rsc.io/xstd/go1.14.14/strings"
 )
 
 var runeMergeTests = []struct {
